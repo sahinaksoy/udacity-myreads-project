4ed254e89e71ad2419a7400ae432ade968391e7f
sahinaksoy <sahinaksoy@gmail.com>
2020-04-28 23:25:37 +0300

    feat: crate search book component This is a container component.
    Send word to api and show search results.
    
    Responsibilities
    -Send word to api
    
    -Show search results
    
    -Update book shelf

---
+++ src/components/SearchBook.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const SearchBook = () => {
+  return (
+    <div className="search-books">
+      {/* TODO: iplement SearchBookBar component */}
+      {/* TODO: iplement SearchBookBarResults component */}
+    </div>
+  );
+};
+
+export default SearchBook;
