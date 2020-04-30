8b6e96e36adfcbd7efacb77a6533176bf57db895
sahinaksoy <sahinaksoy@gmail.com>
2020-05-01 00:02:39 +0300

    feat: get search results from api and set state

--- src/components/SearchBook.js
+++ src/components/SearchBook.js
@@ -2,18 +2,22 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import SearchBookBar from "./SearchBookBar";
 import SearchBookResults from "./SearchBookResults";
-
+import { search } from "../BooksAPI";
 class SearchBook extends Component {
+  state = {
+    books: [],
+  };
 
   handleSearchBook = (value) => {
-    // TODO: add search functionality
+    search(value).then((books) => this.setState({ books }));
   };
 
   render() {
+    const { books } = this.state;
     return (
       <div className="search-books">
         <SearchBookBar onSearchBook={this.handleSearchBook} />
-        <SearchBookResults />
+        <SearchBookResults books={books} />
       </div>
     );
   }
