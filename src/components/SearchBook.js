791d55fc48c8212d297fdf5c245175d2150eb85c
sahinaksoy <sahinaksoy@gmail.com>
2020-04-30 23:45:50 +0300

    feat: iplement SearchBookResults component

--- src/components/SearchBook.js
+++ src/components/SearchBook.js
@@ -1,15 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 import SearchBookBar from "./SearchBookBar";
+import SearchBookResults from "./SearchBookResults";
 
 const SearchBook = () => {
+
   const handleSearchBook = (value) => {
     // TODO: add search functionality
   };
+  
   return (
     <div className="search-books">
       <SearchBookBar onSearchBook={handleSearchBook} />
-      {/* TODO: iplement SearchBookBarResults component */}
+      <SearchBookResults />
     </div>
   );
 };
