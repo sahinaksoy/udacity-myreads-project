6dcd3cf3398c1923c2b18738a7744147cfc05835
sahinaksoy <sahinaksoy@gmail.com>
2020-05-03 16:10:22 +0300

    fix: empty author handle

--- src/components/BookCardFooter.js
+++ src/components/BookCardFooter.js
@@ -6,14 +6,18 @@ const BookCardFooter = (props) => {
   return (
     <>
       <div className="book-title">{title}</div>
-      <div className="book-authors">{authors.join(",")}</div>
+      {authors.length > 0 && (
+        <div className="book-authors">{authors.join(",")}</div>
+      )}
     </>
   );
 };
 
 BookCardFooter.propTypes = {
   title: PropTypes.string.isRequired,
-  authors: PropTypes.arrayOf(PropTypes.string).isRequired,
+  authors: PropTypes.arrayOf(PropTypes.string),
 };
-
+BookCardFooter.defaultProps = {
+  authors: [],
+};
 export default BookCardFooter;
