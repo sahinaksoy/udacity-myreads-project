7a96c5e20e659714010033b381796c6c40d78c52
sahinaksoy <sahinaksoy@gmail.com>
2020-05-03 15:21:36 +0300

    feat: handle search result

--- src/components/SearchBook.js
+++ src/components/SearchBook.js
@@ -3,23 +3,50 @@ import PropTypes from "prop-types";
 import SearchBookBar from "./SearchBookBar";
 import SearchBookResults from "./SearchBookResults";
 import { search } from "../BooksAPI";
+
 class SearchBook extends Component {
   state = {
-    books: [],
+    searchResult: [],
   };
 
-  handleSearchBook = (value) => {
-    search(value).then((books) => this.setState({ books }));
+  handleOnSearchBook = (value) => {
+    const { books } = this.props;
+    search(value.trim()).then((response) => {
+      if (response.error) {
+        this.setState({
+          searchResult: [],
+        });
+      } else {
+        this.setState({
+          searchResult: response.map((book) => {
+            const existBook = books.filter((x) => x.id === book.id);
+            //book shelf update if there is exist shelf
+            if (existBook.length > 0) {
+              book.shelf = existBook[0].shelf;
+            } else {
+              book.shelf = "none";
+            }
+            return book;
+          }),
+        });
+      }
+    });
   };
 
   render() {
-    const { books } = this.state;
+    const { onShelfChange } = this.props;
+    const { searchResult } = this.state;
     return (
       <div className="search-books">
-        <SearchBookBar onSearchBook={this.handleSearchBook} />
-        <SearchBookResults books={books} />
+        <SearchBookBar onSearchBook={this.handleOnSearchBook} />
+        <SearchBookResults books={searchResult} onShelfChange={onShelfChange} />
       </div>
     );
   }
 }
+
+SearchBook.propTypes = {
+  onShelfChange:PropTypes.func
+};
+
 export default SearchBook;
