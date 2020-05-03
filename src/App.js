08f67ea50da268adbd1373a62044b3f96c889ad1
sahinaksoy <sahinaksoy@gmail.com>
2020-05-03 15:16:23 +0300

    feat: iplement search book component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 // import * as BooksAPI from './BooksAPI'
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { getAll, update } from "./BooksAPI";
 import MyReads from "./components/MyReads";
 import SearchBook from "./components/SearchBook";
@@ -17,20 +17,16 @@ class BooksApp extends React.Component {
       })
     );
   }
-  handleShelfChange = (values) => {
-    update({ id: values.id }, values.shelf).then((response) => {
-      console.log(response);
+  handleShelfChange = (changedBook) => {
+    update(changedBook, changedBook.shelf).then((response) => {
       this.setState((prevState) => ({
-        books: prevState.books.map((book) => {
-          //update book shelf value in state
-          if (book.id == values.id) {
-            book.shelf = values.shelf;
-          }
-          return book;
-        }),
+        books: prevState.books
+          .filter((book) => book.id !== changedBook.id)
+          .concat(changedBook),
       }));
     });
   };
+
   render() {
     const { books } = this.state;
     return (
@@ -44,7 +40,15 @@ class BooksApp extends React.Component {
                 <MyReads books={books} onShelfChange={this.handleShelfChange} />
               )}
             />
-            <Route exact path="/search" component={SearchBook} />
+            <Route
+              path="/search"
+              render={() => (
+                <SearchBook
+                  books={books}
+                  onShelfChange={this.handleShelfChange}
+                />
+              )}
+            />
           </Switch>
         </Router>
       </div>
